4517756779b9e0bb24f642baf23f61b3b15e050b
g <2977535920@qq.com>
2020-08-01 15:05:51 +0800

    初始

---
+++ src/main/java/com/hr/S4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S4ProjectApplication.class, args);
+    }
+
+}
