b0921e56e377a6954bad27a24363f8c187c9d9ea
g <2977535920@qq.com>
2020-08-01 15:34:04 +0800

    更改数据库

---
+++ src/main/java/com/hr/controller/AoaUserController.java
@@ -0,0 +1,18 @@
+package com.hr.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName：AoaUserController
+ * @Description：TODO
+ * @Author：HuangZhen
+ * @Date 2020/8/1 15:28
+ * @Versopn：1.0
+ */
+
+@RestController
+public class AoaUserController {
+
+
+
+}
--- src/main/java/com/hr/mapper/AoaUserMapper.java
+++ src/main/java/com/hr/mapper/AoaUserMapper.java
@@ -4,4 +4,5 @@ package com.hr.mapper;
 public interface AoaUserMapper {
 
 
+
 }
