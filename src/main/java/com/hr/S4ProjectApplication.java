081a6b4453ba2ee2985d589855c38507d621b050
g <2977535920@qq.com>
2020-08-01 15:25:21 +0800

    创建user表

--- src/main/java/com/hr/S4ProjectApplication.java
+++ src/main/java/com/hr/S4ProjectApplication.java
@@ -1,9 +1,11 @@
 package com.hr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.hr.mapper")
 public class S4ProjectApplication {
 
     public static void main(String[] args) {
